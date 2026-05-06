9784ac9177f0e9e3d276e92f325264d38becf71c
JoashYeoh <joashyeoh@gmail.com>
2026-05-06 19:21:38 +0800

    update backlog 1 sprint 2

--- combined_osc/osc_combined_functions.py
+++ combined_osc/osc_combined_functions.py
@@ -17,7 +17,7 @@ def send_message(address: str, message: str):
 
 
 # GrandMA Functions
-addr = "/grandma/gma3/cmd"
+addr = "/gma3/cmd"
 
 # Math Function to convert Values to Command line Values to GMA3 Values e.g. pan; -315 = 0 = -315, pan; 315 = 65535 = 315
 def GMA3_Value_Converter(y, y_min, y_max, x_min=0, x_max=65535):
@@ -55,27 +55,37 @@ def gma_dimmer(fixture: str, value: int):
 msg = float(1) # Trigger TRUE Value
 
 def reaper_play():
-    send_message("/reaper/action/1007", msg)
+    send_message("/action/1007", msg)
 
 def reaper_pause():
-    send_message("/reaper/action/1008", msg)
+    send_message("/action/1008", msg)
 
 def reaper_play_pause():
-    send_message("/reaper/action/40073", msg)
+    send_message("/action/40073", msg)
 
 def reaper_stop():
-    send_message("/reaper/action/1016", msg)
+    send_message("/action/1016", msg)
 
 def reaper_play_stop():
-    send_message("/reaper/action/40044", msg)
+    send_message("/action/40044", msg)
 
 def reaper_marker(marker_no: int):
     #addr = "/action/40161" # Jump to Marker One
     if marker_no <= 10:
-        send_message(f"/reaper/action/4016{str(marker_no)}", msg)
+        send_message(f"/action/4016{str(marker_no)}", msg)
     elif marker_no > 10:
         return print("reaper marker error. Please input a marker ID between 1 to 10")
+    
 
+def handle_input(source, command):
+    if command == "cue1":
+        reaper_marker(1)
+    elif command == "cue2":
+        reaper_marker(2)
+    elif command == "pause":
+        reaper_pause()
+    elif command == "play":
+        reaper_play()
 
 
 
@@ -85,7 +95,7 @@ def lisa_pan(degrees):
     degrees = max(-180.0, min(180.0, float(degrees)))
     pan = (degrees + 180.0) / 360.0
 
-    addr = "/lisa/ext/src/1/p"
+    addr = "/ext/src/1/p"
     send_message(addr, pan)
 
 def lisa_distance(percent): 
@@ -93,16 +103,17 @@ def lisa_distance(percent):
     percent = max(0.0, min(100.0, float(percent)))
     distance = percent / 100.0
 
-    addr = "/lisa/ext/src/1/d"
+    addr = "/ext/src/1/d"
     send_message(addr, distance)
 
 def lisa_fx(fx):
-    addr = "/lisa/ext/src/1/send/1"
+    addr = "/ext/src/1/send/1"
     send_message(addr, fx)
 
 
-
-# ───────────────── Call Functions ─────────────────
+# ────────────────── BACKLOG 1 SPRINT 1 ──────────────────
+# ───────── CALLING OF FUNTIONS ─────────
+"""
 lisa_pan(180)  #lisa_pan(float(1)) # 0 -> -180deg, 0.5 -> 0deg,  1 -> 180deg
 lisa_distance(60)  # 0 -> 0%, 1 -> 100%
 lisa_fx(float(0))  # 0 -> off, 0.5 -> -6dB,  1 -> 0dB
@@ -118,3 +129,32 @@ gma_cue(2, 1)  # goto cue __ on sequence __
 
 reaper_marker(2)  # jump to maker __
 reaper_play() 
+"""
+
+# ────────────────── BACKLOG 1 SPRINT 2 ──────────────────
+# ───────── CALLING OF FUNTIONS ─────────
+
+# From 00:00:00:00
+handle_input("test", "play") # Reaper Start tracks
+gma_cue(1,1) # Trigger cue 1 of sequence 1
+time.sleep(7)
+
+handle_input("test", "pause") # Reaper Pause
+time.sleep(3)
+
+## Makrer 1
+handle_input("test", "cue1") # Reaper Jump to marker 1
+handle_input("test", "play") # Reaper continue lay tracks
+gma_cue(2,1) # Trigger cue 2 of sequence 1
+time.sleep(10)
+
+handle_input("test", "pause") # Reaper Pause
+time.sleep(3)
+
+## Makrer 2
+handle_input("test", "cue2") # Repaer Jump to marker 2
+handle_input("test", "play") # Reaper continue lay tracks
+gma_cue(3,1) # Trigger cue 3 of sequence 1
+time.sleep(10)
+
+handle_input("test", "pause") # Reaper pause (end of show)
