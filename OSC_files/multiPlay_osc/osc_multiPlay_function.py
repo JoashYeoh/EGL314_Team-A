b960b4fb9a42fb7ed44f8a0310f1b3adea7819a2
JoashYeoh <joashyeoh@gmail.com>
2026-06-04 10:34:13 +0800

    multiplay osc update

--- OSC_files/multiPlay_osc/osc_multiPlay_function.py
+++ OSC_files/multiPlay_osc/osc_multiPlay_function.py
@@ -2,24 +2,22 @@
 # This python script can be used to control Multiplay 3 software
 # For more infomation, please refer to 
 # http://da-share.com/help/multiplay3/OSC-Control-Cue-Actions.html
-import socket
+from pythonosc import udp_client
 
-def send_message(IP, Port, Message):
-
+def send_message(receiver_ip, receiver_port, address, message):
   try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    MESSAGE = bytes(Message, 'UTF-8')
-    sock.sendto(MESSAGE, (IP, Port))
-    sock.close()
-    print(f'messsage sent: {Message}')
+    # Create an OSC client to send messages
+    client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
+
+    # Send an OSC message to the receiver
+    client.send_message(address, message)
+
+    print("Message sent successfully.")
   except:
-    print(f'message not sent: {Message}')
+    print("Message not sent")
 
+# FOR INFO: IP address and port of the receiving Raspberry Pi
+PI_A_ADDR = "192.168.254.189"    # wlan ip
+PORT = 8888 
 
-if __name__ == "__main__":
-# UDP_IP is target IP address
-  IP = "192.168.254.189" #Local Host Address
-  PORT = 8888
-  message = "/cue/1/go" # Trigger Cue 1
-
-  send_message(IP, PORT, message)
+send_message(PI_A_ADDR, PORT, "/cue/1/go", "")
