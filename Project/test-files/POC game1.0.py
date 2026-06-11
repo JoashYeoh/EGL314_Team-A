c569282c38d4d8ad044f09bc9a7f5204c95848b4
EdisonLeo216 <edisonlwa06@gmail.com>
2026-06-11 11:03:24 +0800

    photo size fix

--- Project/test-files/POC game1.0.py
+++ Project/test-files/POC game1.0.py
@@ -642,9 +642,9 @@ def process_zone_transitions(tag_id, tag):
 # Viewer App
 # Tutorial/Instructions for game
 #----------------------------------------------------------------------------
-class TutorialWindow:
-    def __init__(self, parent, state, fullscreen):
-        self.parent = parent
+class TutorialWindow: #defines class handling intruction window
+    def __init__(self, parent, state, fullscreen): #initialization and take parent and app to communicate with main game
+        self.parent = parent #Saves these window references as object variables so any function inside this class can access them later.
         self.state = state
         self.fullscreen = fullscreen
         
@@ -661,14 +661,14 @@ class TutorialWindow:
 
         # Define the instruction pages (Text + optional placeholder image file)
         self.pages = [
-            {"text": "1. Welcome to Zone Capturing. click next to view how to play the game.", "img": "Assets/step1.png"},
+            {"text": "1. Welcome to Zone Capturing. click next to view how to play the game.", "img": "Assets/Step67.png"},
             {"text": "2. The objective of this game is to capture all safe zones, by standing withitn the zone.", "img": "Assets/step2.png"},
             {"text": "3. Upon reaching a safe zone, you have to remain in the zone, as capturing commences! (zone stops expanding when captured)", "img": "Assets/step3.png"},
             {"text": "4. Once all safe zones have been captured successfully, you will progress to the next round.", "img": "Assets/step4.png"},
             {"text": "5. However, beware of the DANGER ZONES. AVOID THEM AT ALL COST! Coming into contact with them would end the game.", "img": "Assets/step5.png"},
             {"text": "6. There will be two rounds in total. In the second round, the speed of the moving danger zones increases!", "img": "Assets/step6.png"},
             {"text": "7. Leaving the safe zones, will cause the safe zones to shrink. STAY ON IT!", "img": "Assets/step7.png"},
-            {"text": "8. That's it! Are you ready to take on the challenge explorer? If you are, click on 'start game'.", "img": "Assets/step8.png"}
+            {"text": "8. That's it! Are you ready to take on the challenge explorer? If you are, click on 'start game'.", "img": "Assets/Step68.png"}
         ]
         self.current_page = 0
 
@@ -793,9 +793,9 @@ class TutorialWindow:
         # Only after Tutorial Window is destroyed would the ViewerApp (game) run
         ViewerApp(self.parent, self.state, True, self.fullscreen)
 
-        self.parent.deiconify()
+        self.parent.deiconify() #show the game window after the instruction page closes
         self.parent.lift()
-        self.parent.focus_force()
+        self.parent.focus_force() #pull focus onto the game application so keybinds work immediately at the game window
 
     def on_close(self):
         # Force clean terminate on early cancellation exit routines
